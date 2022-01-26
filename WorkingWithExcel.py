b56410f84cb7fc47342e27088e35e1ec99c09437
macavelli <54381776+isiahturner@users.noreply.github.com>
2022-01-26 10:41:27 -0500

    commit

---
+++ WorkingWithExcel.py
@@ -0,0 +1,10 @@
+
+# * The purpose of this code is to import an excel file, access the imported data and then create a function that
+# * finds the last names beginning with 'a' while counting how many of the list start with 'a'. Will be using
+# * pandas processes to get more practice using it.
+
+
+# Importing the data
+
+
+def lastNameTracker(series, desiredLetter):
